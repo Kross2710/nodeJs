4b6f25e98be870f2172095905aa4e04f511c0beb
Hung Vu <138547805+Kross2710@users.noreply.github.com>
2024-05-18 15:31:26 +0700

    Update view engine and static file configuration

--- src/config/viewEngine.js
+++ src/config/viewEngine.js
@@ -1,8 +1,14 @@
 const path = require('path');
+const express = require('express');
 
 const configViewEngine = (app) => {
+    // config template engine
+    console.log('check __dirname', __dirname);
     app.set('views', path.join(__dirname, '../views'));
     app.set('view engine', 'ejs');
+
+    // config static files
+    app.use(express.static(path.join(__dirname, '../public')));
 };
 
 module.exports = configViewEngine;
--- src/server.js
+++ src/server.js
@@ -7,11 +7,7 @@ const port = process.env.PORT || 8888;
 const hostname = process.env.HOST_NAME;
 
 // config template engine
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
-
-// config static files
-app.use(express.static(path.join(__dirname, 'public')));
+configViewEngine(app);
 
 // khai bao route
 app.get('/', (req, res) => {
