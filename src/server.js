68954289260e44cb7d40b3ee60de894f1bf66eff
Knorr2710 <138547805+Knorr2710@users.noreply.github.com>
2024-05-19 17:14:11 +0700

    Update console.log statements in server.js

--- src/server.js
+++ src/server.js
@@ -30,8 +30,8 @@ const connection = mysql.createConnection({
 connection.query(
   'SELECT * FROM Users',
   function(err, results, fields) {
-    console.log(results); // results contains rows returned by server
-    console.log(fields); // fields contains extra meta data about results, if available
+    console.log(">>> ", results); // results contains rows returned by server
+    console.log(">>> ", fields); // fields contains extra meta data about results, if available
   }
 );
 
