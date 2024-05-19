996a74dee26a0e5f119ec0573bea5c65a30fc080
Knorr2710 <138547805+Knorr2710@users.noreply.github.com>
2024-05-19 14:54:53 +0700

    chore: Update npm dependencies for view engine and static file configuration

--- src/server.js
+++ src/server.js
@@ -4,6 +4,8 @@ require('dotenv').config(); // load env file
 const configViewEngine = require('./config/viewEngine');
 const webRoutes = require('./routes/web');
 
+const mysql = require('mysql2');
+
 const app = express();
 const port = process.env.PORT || 8888;
 const hostname = process.env.HOST_NAME;
@@ -13,6 +15,25 @@ configViewEngine(app);
 
 // khai bao route
 app.use('/', webRoutes);
+
+// test connection
+// create the connection to database
+const connection = mysql.createConnection({
+  host: "localhost",
+  user: "root", // default: empty
+  password: "123456",
+  port: 3307, // default 3306
+  database: "hoidanit"
+});
+
+// simple query
+connection.query(
+  'SELECT * FROM Users',
+  function(err, results, fields) {
+    console.log(results); // results contains rows returned by server
+    console.log(fields); // fields contains extra meta data about results, if available
+  }
+);
 
 app.listen(port, hostname, () => {
   console.log(`Example app listening at http://localhost:${port}`);
